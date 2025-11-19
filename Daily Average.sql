32b62cf2955969e1d3088fef4b8501d54f6f5978
Everett K McDonough <everett.k.mcdonough@sherwin.com>
2025-11-19 09:09:39 -0500

    Initial commit of Snowflake queries

---
+++ Daily Average.sql
@@ -0,0 +1,18 @@
+WITH daily_usage AS (
+    SELECT
+        warehouse_name,
+        DATE_TRUNC('day', start_time::date) AS day,
+        SUM(credits_used) AS daily_credits
+    FROM SNOWFLAKE.ACCOUNT_USAGE.WAREHOUSE_METERING_HISTORY
+    WHERE start_time >= DATEADD(MONTH, -4, DATE_TRUNC('day', CURRENT_DATE))
+    AND start_time < dateadd('day',-1,date_trunc('day',CURRENT_DATE::date))
+     -- AND warehouse_name = 'FINOPS_WH'
+    GROUP BY 1,2
+)
+SELECT
+    warehouse_name,
+    DATE_TRUNC('month', day) AS month,
+    AVG(daily_credits) AS avg_daily_credits_per_month
+FROM daily_usage
+GROUP BY warehouse_name, DATE_TRUNC('month', day)
+ORDER BY month;
